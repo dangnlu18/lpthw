d9fd6fb672f483df9e6c14b6671055c85238939c
dang lu <dangnlu18@gmail.com>
2021-03-14 15:39:02 -0700

    init commit

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first , second, third = argv
+
+print(f"The script is called: {script}")
+print(f"Your first variable is called: {first}") 
+print(f"Your second variable is called: {second}")
+print(f"Your third variable is called: {third}")
